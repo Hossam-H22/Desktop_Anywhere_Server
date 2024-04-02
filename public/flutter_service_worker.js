1f2d457eed6f84b2fa6724a4e7fb81a5bfa1187b
Hossam Hatem <hosam.hatem222@gmail.com>
2024-04-02 02:43:56 +0200

    edit

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1ba14034c217fe903cbe9abe511e2e3d",
-"/": "1ba14034c217fe903cbe9abe511e2e3d",
-"main.dart.js": "1c972ff6c743b2fe1d6bed0721da29a6",
+"index.html": "b515987bbfecb16ef8c9836071c39cec",
+"/": "b515987bbfecb16ef8c9836071c39cec",
+"main.dart.js": "7f017c00e1f41fa47040ec0364c32e4c",
 "manifest.json": "ae2d5fe3887adca50b0979ca58709f4a",
 "version.json": "96f951263420c1d99dc8b0b39f96be8f"};
 // The application shell files that are downloaded before a service worker can
