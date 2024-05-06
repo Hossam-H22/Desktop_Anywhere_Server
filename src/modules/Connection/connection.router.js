fc7a37d871a07341516d8246c5db108b601090e1
Hossam Hatem <hosam.hatem222@gmail.com>
2024-05-06 22:56:29 +0300

    add connection end points

--- src/modules/Connection/connection.router.js
+++ src/modules/Connection/connection.router.js
@@ -9,6 +9,6 @@ router.get("/", connectionController.getAll);
 router.get("/desktop/:desktop_mac", connectionController.getDesktopConnections);
 router.get("/mobile/:mobile_id", connectionController.getMobileConnections);
 router.post("/", connectionController.addConnection);
-router.delete("/:id", connectionController.deleteCallee);
+router.delete("/:id", connectionController.deleteConnection);
 
 export default router;
--- src/modules/Connection/controller/connection.js
+++ src/modules/Connection/controller/connection.js
@@ -1,4 +1,5 @@
 
+import mobileModel from '../../../../DB/Models/Mobile.model.js';
 import connectionModel from './../../../../DB/Models/Connection.model.js';
 import { asyncHandler } from './../../../utils/errorHandling.js';
 
@@ -12,8 +13,14 @@ export const getAll = asyncHandler(async (req, res, next) => {
 
 export const getDesktopConnections = asyncHandler(async (req, res, next) => {
     const mobiles = await connectionModel.find({ 'desktop_mac': req.params.desktop_mac });
-    return res.status(200).json({ message: "Done", mobiles });
+    var mobileIds = [];
+    mobiles.forEach(element => {
+        mobileIds.push(element.mobile_id);
+    });
+    const mobilesData = await mobileModel.find({ '_id': mobileIds });
+    return res.status(200).json({ message: "Done", mobiles: mobilesData });
 })
+
 
 export const getMobileConnections = asyncHandler(async (req, res, next) => {
     const desktops = await connectionModel.find({ 'mobile_id': req.params.mobile_id });
@@ -36,8 +43,8 @@ export const addConnection = asyncHandler(async (req, res, next) => {
 })
 
 
-export const deleteCallee = asyncHandler(async (req, res, next) => {
-    const connection = await connectionModel.deleteOne({ 'id': req.params.id });
+export const deleteConnection = asyncHandler(async (req, res, next) => {
+    const connection = await connectionModel.deleteOne({ '_id': req.params.id });
     return connection.deletedCount ? res.status(200).json({ message: "Done"}) 
                     : next(new Error(`In-valid or Not Found ID: ${req.params.id}`, {cause: 404}));
 })
